60b5e2f125abfe222e5765d3090ee5888be28353
crabtsai <87925842+crabtsai@users.noreply.github.com>
2023-08-27 08:17:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ from tensorflow.keras.models import model_from_json
 
 # 模型載入
 model = tf.keras.models.load_model('model.h5')
-
-col1, col2 = st.columns(2)
 st.title('Crab_數字及A~Z辨識')
+col1, col2 = st.columns(2)
+
 with col1:
     # Create a canvas component
     canvas_result = st_canvas(
